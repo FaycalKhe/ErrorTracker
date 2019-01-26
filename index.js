96e5b32f60fd164ab4d90661740e853966c77972
Faycel <faycal.khe@gmail.com>
2019-01-26 15:42:09 +0100

    adding the body parser

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const app = require('express')();
+const bodyParser = require('body-parser');
+
+// Adding the body parser middel ware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 
 app.listen(3000, () => console.log('start listning on port 3000'));
